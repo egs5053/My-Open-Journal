18361aec4700650aab614be586b45b836638bad2
egs5053 <egs4043@gmail.com>
2013-12-04 18:48:15 -0800

    Added email password code functionality to password reset.

--- workspace/My Open Journal/src/Test/Reset.java
+++ workspace/My Open Journal/src/Test/Reset.java
@@ -1,10 +1,13 @@
 package Test;
 
+
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.event.EventListener;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.*;
 import org.zkoss.zul.Messagebox;
+import org.zkoss.zul.Messagebox.ClickEvent;
 import org.zkoss.zul.Textbox;
 
 public class Reset extends SelectorComposer<Component>{
@@ -13,6 +16,8 @@ public class Reset extends SelectorComposer<Component>{
 	@Wire 
 	Textbox username;
 	
+	// Checks to see if the password matches the username. If so it
+	// logs the user in and redirects to homepage
     @Listen("onClick = #reset")
     public void LoginUser(){
     	DBManager manager = new DBManager();
@@ -20,13 +25,24 @@ public class Reset extends SelectorComposer<Component>{
     	// Check to see if username is already registered
     	if(manager.IsValidUser(user)) {
     		int id = manager.GetID(user);
+    		
+    		EventListener<ClickEvent> clickListener = new EventListener<Messagebox.ClickEvent>() {
+				public void onEvent(ClickEvent event)
+				{
+					Executions.sendRedirect("index.zul");
+				}
+    		};
+    		
     		if(manager.ResetPasswordKey(id)){
-                Messagebox.show("Password key generated!");
+    			int resetCode = manager.GetResetCode(manager.GetID(user));
     			//Add email sending code
+    			Email.SendPasswordReset(manager.GetEmail(user), resetCode);
+    			Messagebox.show("Your password code has been sent to your email.");
     		}
     		else{
-    			Messagebox.show("There was an error resetting your password");
-    		}   	
+    			Messagebox.show("Your password code has been sent to your email.");
+    		}
+    		
     	}
     	else
 			Messagebox.show("Invalid username!!");    	
